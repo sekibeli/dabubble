bc28a5a79b5d7c140038ac195e5017c14163295f
Julia Georgiew <java2011@me.com>
2023-08-09 19:10:57 +0200

    postheader

--- src/app/postheader/postheader.component.ts
+++ src/app/postheader/postheader.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ChannelService } from '../services/channel.service';
 import { BehaviorSubject } from 'rxjs';
 import { UserService } from '../services/user.service';
-import { identifierName } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-postheader',
@@ -13,42 +13,43 @@ export class PostheaderComponent implements OnInit {
 
   activeChannelTitle = 'Angular';
   activeChannelID: BehaviorSubject<string> =   new BehaviorSubject<string>('9Gwz1Ce763caWx5FCBZL');
-  members: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  members;
+  countsOfMembers;
+  currentChannelUser;
   constructor(public channelService: ChannelService, public userService: UserService) {
-   
-  }
+    this.channelService.activeChannelID.subscribe((value)=> {
+      this.activeChannelID.next(value);
+     
+    });
+ }
+    
+  
 
   ngOnInit() {
+      // this.channelService.currentChannelUser.subscribe((user)=>{
+      //  this.currentChannelUser
+      // });
+
     this.channelService.activeChannelTitle.subscribe(
       (value) => {
         this.activeChannelTitle = value;
       } );
-      this.channelService.getMembers(this.activeChannelID.getValue()).subscribe((initialMembers) => {
-        this.members.next(initialMembers);
-    });
-  
+ 
+      this.channelService.channelUserArrayEmitter.subscribe((users)=> {
+        console.log(users);
+        this.members = users;
+        this.countsOfMembers = this.members.length;
+      });
+     
 
-    this.channelService.activeChannelID.subscribe((newActiveChannelID) => { 
-
-      //  this.channelService.getMembersTest(newActiveChannelID)
-      
-      this.channelService.getMembersTest(newActiveChannelID).subscribe((value)=>{
-        console.log(value);
-        this.members.next(value);
-        console.log(this.members.getValue());
-       });
-      // this.channelService.getMembers(newActiveChannelID).subscribe((value)=> {
-      //   this.members.next(value);
-      //   console.log(this.members.getValue());
-      // });
-    });
+    // this.channelService.activeChannelID.subscribe((newActiveChannelID) => { 
+    //          this.channelService.getMembersTest(newActiveChannelID).subscribe((value)=>{
+    //     this.members = value;
+       
+    //    });
+   
+    // });
   
   }
-
-  
-
-
-  
-   
 
 }
--- src/app/services/channel.service.ts
+++ src/app/services/channel.service.ts
@@ -1,18 +1,19 @@
-import { EventEmitter, Injectable, OnInit, inject } from '@angular/core';
+import { EventEmitter, Injectable, OnDestroy, OnInit, inject } from '@angular/core';
 import { Firestore, collection, collectionData, doc, docData } from '@angular/fire/firestore';
 import { UserService } from './user.service';
 import { BehaviorSubject, merge, mergeAll } from 'rxjs';
+import { User } from '../models/user.class';
 
 @Injectable({
    providedIn: 'root'
 })
-export class ChannelService implements OnInit {
+export class ChannelService implements OnInit, OnDestroy {
    firestore: Firestore = inject(Firestore)
    channelUser: BehaviorSubject<any[]> = new BehaviorSubject<any[]>(null);
-   // channelID: string ;
+   channelUserIDArray;
+   channelUserArrayEmitter = new EventEmitter<any>();
 
-   // channels;
-
+   currentChannelUserArray;
    activeChannelTitle = new EventEmitter<string>();
    activeChannelID = new EventEmitter<string>();
 
@@ -31,43 +32,81 @@ export class ChannelService implements OnInit {
    }
    pushActiveChannel(title, id) {
       this.activeChannelTitle.emit(title);
-      this.activeChannelID.emit(id);
-
+     
+      this.getMembersOfChannel(id).subscribe((value)=> {
+         this.channelUserIDArray = value;
+         this.getMembersData(this.channelUserIDArray);
+         console.log('der erste Output:', this.channelUserIDArray);
+      });
+   
    }
 
+getMembersData(userArray){
+   // console.log('Länge', userArray);
+   let fetchCount = userArray.length;
+  let usersArray = [];
+  
+   userArray.forEach(element => {
+      this.userService.getCurrentUser(element['id']).subscribe((user) => {
+         usersArray.push(user);
+         fetchCount--;
+   
+         if (fetchCount === 0) {
+            this.currentChannelUserArray = usersArray;
+            console.log('zweiter Output', this.currentChannelUserArray);
+           this.channelUserArrayEmitter.emit(this.currentChannelUserArray);
+         }
+   
+      });
+   });
 
-   getMembers(channel) {
+  
+
+}
+
+
+
+   getMembersOfChannel(channel) {
       const collRef = collection(this.firestore, 'channels', channel, 'members');
       const docRef = collectionData(collRef, { idField: 'id' });
       return docRef;
 
    }
 
-   getMembersTest(channel) {
-      let usersArray = [];
+   // getMembersTest(channel) {
+   //    let usersArray = [];
 
-      const collRef = collection(this.firestore, 'channels', channel, 'members');
-      const docRef = collectionData(collRef, { idField: 'id' });
+   //    const collRef = collection(this.firestore, 'channels', channel, 'members');
+   //    const docRef = collectionData(collRef, { idField: 'id' });
 
-      docRef.subscribe((value) => {
-         let fetchCount = value.length;
+   //    docRef.subscribe((value) => {
+   //       let fetchCount = value.length;
 
-         value.forEach((element) => {
+   //       value.forEach((element) => {
        
-            this.userService.getCurrentUser(element['id']).subscribe((user) => {
-               usersArray.push(user);
-               fetchCount--;
+   //         this.userService.getCurrentUser(element['id']).subscribe((user) => {
+   //             usersArray.push(user);
+   //             fetchCount--;
 
-               if (fetchCount === 0) {
-                  this.channelUser.next(usersArray);
-               }
+   //             if (fetchCount === 0) {
+   //                this.channelUser.next(usersArray);
+                 
+   //             }
 
-            });
+   //          });
 
-         });
-      }
-      )
-      return this.channelUser;
+   //       });
+   //    }
+   //    )
+   //    this.currentChannelUser.emit(this.channelUser)
+   //    console.log('channeluser:', this.channelUser);
+     
+   //    return this.channelUser;
+   // }
+
+   ngOnDestroy(): void {
+   
+      console.log('zerstört');
    }
 
 }
